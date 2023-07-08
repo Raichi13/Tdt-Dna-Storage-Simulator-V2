f9e7f94b2d1af0f534baa579fcf6af8c7e64de68
Raichi13 <m@raichi.nl>
2023-07-08 18:34:58 +0900

    add params

--- params_gen.py
+++ params_gen.py
@@ -8,9 +8,9 @@ params = []
 #     for j in range(50):
 #         params.append([250,'r','1k_data',32,4,i + 1,0.002,0.0,0.0,j + 1,3])
 for i in range(100):
-    params.append([250,'d','1k_data',32,4,1,0.001,0.0,0.0,i + 1,3])
+    params.append([250,'d','1k_data',16,4,1,0.01,0.0,0.0,i + 1,3])
 
-with open('sim19.csv', 'w') as f:
+with open('sim21.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(header)
     writer.writerows(params)
